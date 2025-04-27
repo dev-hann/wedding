df9fd17f1a7db04861172df4f11d771ccf642c5c
hann <hann.yun@langdy.net>
2025-04-27 17:41:59 +0900

    🚧 Chore: ignore

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,10 @@
 import type { NextConfig } from "next";
-const isProduction = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  assetPrefix: isProduction ? "/wedding/" : "",
-  output: "export",
   images: {
     unoptimized: true,
   },
-  devIndicators: false,
-  trailingSlash: true,
 };
+
 export default nextConfig;
